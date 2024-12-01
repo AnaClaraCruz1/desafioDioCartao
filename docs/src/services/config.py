09e6b1a47cb18d5b0f9ff848764d451026419cd5
AnaClaraCruz1 <146086298+AnaClaraCruz1@users.noreply.github.com>
2024-11-30 23:59:24 -0300

    alteração em config.py

--- docs/src/services/config.py
+++ docs/src/services/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 class Config:
     ENDPOINT = ""
     SUBSCRIPTION_KEY = ""
-    AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=diolabsdocsdeveastus001;AccountKey=wGIf0Cd/nFXGuTxzDUnONCTOlSd12moqrbLUbe/KL1cwB+ncauiDCO6QL6TI7kOipWS8O9F8ln6D+AStYmTMbg==;EndpointSuffix=core.windows.net "
+    AZURE_STORAGE_CONNECTION_STRING = ""
     CONTAINER_NAME = "cartoes"
